638b02e9c6cb42aff39fee8442f5160463c01e9e
Arun Kumar Dhuraisamy <arundhuraisamy270@gmail.com>
2024-12-03 23:31:46 +0100

    trying out with allowing labels with all zeros

--- dataset.py
+++ dataset.py
@@ -120,8 +120,12 @@ class BrightfieldMicroscopyDataset(Dataset):
         label = Image.open(label[0])
         label = np.array(label)
 
-        if not images.any() or not label.any():
-            raise RuntimeError(f"Missing images or labels for index {idx}. Images: {images}, Label: {label}")
+        # if not images.any() or not label.any():
+        #     raise RuntimeError(f"Missing images or labels for index {idx}. Images: {images}, Label: {label}")
+
+        if not images.any():  # Check only if the images are invalid
+            raise RuntimeError(f"Missing images for index {idx}. Images: {images}") # Allow labels with all zeros
+
 
         images = torch.from_numpy(images)
         label = torch.from_numpy(label)
