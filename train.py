601515ed756b88861d8032923748b2df6b294162
hndrkjs <79534280+hndrkjs@users.noreply.github.com>
2024-12-20 09:50:24 +0100

    retrain preprocess base unet

--- train.py
+++ train.py
@@ -246,13 +246,13 @@ def train_model(model, train_loader, val_loader, test_loader, optimiser, lr_sche
 
 if __name__ == '__main__':
 
-    args = parse_args_3dunet()
+    args = parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     torch.backends.cudnn.deterministic = True
 
-    model = BaseUnet3D(num_inputs=11)
+    model = BaseUnet(num_inputs=11)
 
     train_loader, val_loader, test_loader = get_dataloader(args.sample_size, args.batch_size)
 
